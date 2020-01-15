ec82de0579f40b587f7e91b9d04de8f2d2e2fbfb
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-01-15 11:20:05 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = "an_example_pypi_project",
+    version = "0.0.4",
+    author = "Silviu Surcica",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
