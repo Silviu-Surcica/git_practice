471497591787557e2d9c22138442d756c481c112
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-01-15 15:49:32 +0200

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "git_practice_xxx",
-    version = "2.0",
+    version = "2.5",
     author = "Silviu Surcica",
     description = ("An demonstration of how to create, document, and publish "
                                    "to the cheese shop a5 pypi.org."),
