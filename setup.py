db3b23c728fbff1aa8d3ea5ace9ea7cf1990dfad
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-01-15 11:47:24 +0200

    changed conflicting name on pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup
 
 setup(
-    name = "an_example_pypi_project",
+    name = "git_practice_xxx",
     version = "0.0.4",
     author = "Silviu Surcica",
     description = ("An demonstration of how to create, document, and publish "
                                    "to the cheese shop a5 pypi.org."),
     license = "BSD",
-    keywords = "example documentation tutorial",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
 )
