c57dcee12d4141d5cfef377dfbc1e918ec5f3f33
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-01-16 14:18:40 +0200

    feat: take the version from manifest

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
+import yaml
+
+with open('manifest.yml', 'r') as f:
+    version = yaml.safe_load(f).get('version')
 
 setup(
-    name = "git_practice_xxx",
-    version = "2.9.5",
-    author = "Silviu Surcica",
-    description = ("An demonstration of how to create, document, and publish "
+    name="git_practice_xxx",
+    version=version,
+    author="Silviu Surcica",
+    description=("An demonstration of how to create, document, and publish "
                                    "to the cheese shop a5 pypi.org."),
-    license = "BSD",
+    license="BSD",
 )
