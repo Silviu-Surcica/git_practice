401151e1b5263925114f8f87b977d16dcbdb4655
silviu.surcica <silviu.surcica@solarwinds.com>
2020-01-22 12:21:20 +0200

    make tests fail

--- tests/test_1.py
+++ tests/test_1.py
@@ -3,7 +3,7 @@ import unittest
 
 class Test1(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('foo'.upper(), 'FOOx')
 
 
 if __name__ == '__main__':
